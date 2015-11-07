7aa93c865660e9393a949b78a946a4f4a856bb20
ArgeSwarge <argecata@mail.ru>
2015-11-07 17:24:59 +0200

    Arge - Parser
    Main commit

---
+++ parser.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Parser Settings</title>
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+ </head>
+ <body>
+
+<?php
+
+$from = $_POST["from"];
+$where = $_POST["where"];
+$data1 = $_POST["data1"];
+$days = $_POST["days"];
+$data2 = $_POST["data2"];
+
+
+
+
+
+
+?>
+</body>
+	</html>
