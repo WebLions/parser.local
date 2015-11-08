5cb354f0c00eaa8a833a138ff05cd3e6a364047c
ArgeSwarge <argecata@mail.ru>
2015-11-08 16:57:16 +0200

    JS +Index Update
    JS +Index Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 (function($){
 	$(window).load(function(){
 
+	
+	
+	$(".select-parser").click(function(){
+		
+		$("#sform").attr('data-ajax',$(this).attr('data-id'));
+		
+		if(!$(this).hasClass('active')){
+		
+			$(".select-parser").each(function(){
+				
+			$(this).removeClass('active');	
+				
+				
+			});
+			$(this).addClass('active');
+			
+		};
+		
+		
+		return false;		
+	});
+	
+	
 	$("#search").click(function(e){
 		$("#result").html('<img src="/img/238.GIF">');
-
 		
-		$.post("/ajax_flydubai.php", $( "#sform" ).serialize(), function(data){
+		
+		var parser = $("#sform").attr('data-ajax','flydubai');
+		
+		$.post("/ajax_"+parser+".php", $( "#sform" ).serialize(), function(data){
 				$("#result").html(data);
 			});
-		return false;
-		e.preventdefault();
-	});
+			return false;
+			e.preventdefault();
+		});
 
 	});
 })(jQuery);
