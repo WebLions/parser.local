23dbaba7d32fa1a3ec7c52778054418db9919ce6
Igor Kobziev <igorok901@gmail.com>
2015-11-08 16:55:01 +0200

    add parser flydubai

--- ajax_flydubai.php
+++ ajax_flydubai.php
@@ -136,6 +136,8 @@ $header['errmsg'] = $errmsg;
 $header['content'] = $content;
 return $header;
 } 
+
+
 $Origin = $_POST['Origin'];
 $Destination = $_POST['Destination'];
 $first_date = $_POST['first_date'];
@@ -145,7 +147,16 @@ $pback = $_POST['pback'];
 $date = new DateTime($first_date);
 $date->add(new DateInterval('P'.$period.'D'));
 $second_date = $date->format('d/m/Y');
-//echo $second_date;
+
+$date = new DateTime($first_date);
+
+$first_date = $date->format('d');
+$first_date.= '/'.$date->format('m');
+$first_date.= '/'.$date->format('Y');
+$date = new DateTime($first_date);
+$date->modify('+3 day');
+$first_date = $date->format('d/m/Y');
+
 $ReturnDate = $second_date;
 
 $postdata = 'roundSingle=on&';
@@ -180,50 +191,138 @@ if( $curl = curl_init() ) {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     $out = curl_exec($curl);
     preg_match('/RT\(([0-9]+)/', $out, $matches);
-    
     curl_close($curl);
    } 
-
+//echo 'http://flights.flydubai.com/en/results/threedayview/?_='.$matches[1];
 if( $curl = curl_init() ) {
 	$uagent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)";
 	curl_setopt($curl, CURLOPT_URL, 'http://flights.flydubai.com/en/results/threedayview/?_='.$matches[1]);
-	//curl_setopt($curl, CURLOPT_HEADER, true);
+	curl_setopt($curl, CURLOPT_HEADER, true);
 	curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
 	curl_setopt($curl, CURLOPT_USERAGENT, $uagent);
 	curl_setopt($curl, CURLOPT_COOKIEJAR, "coo.txt");
 	curl_setopt($curl, CURLOPT_COOKIEFILE,"coo.txt");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-    $out = curl_exec($curl);
+    $html = curl_exec($curl);
     curl_close($curl);
    } 
-   $out = str_replace(array("\r", "\n", " "),'',$out);
-   $out = str_replace(array("\r", "\n", " "),'',$out);
-   $out = str_replace(array("\r", "\n", " "),'',$out);
+
+  // echo $html;
+  // $out = str_replace(array("\r", "\n", " "),'',$out);
+
+
+  //  preg_match('/USD<br\/>(.*?)<\/div>/', $out, $matches);
+
+  //  preg_match('/TDFare_1_ODS_BEY_(.*?)\'/', $out, $d);
+  // print_r($matches);
+  $html = str_get_html($html);
+  //$am = $html->find('.dayAmount .showdiv span',0);
+  //$am = $am->find('.showdiv span')->innertext;
+
+/*
 $file = 'people.txt';
 // Открываем файл для получения существующего содержимого
 $current = file_get_contents($file);
 // Добавляем нового человека в файл
-$current .= $out;
+$current .= $am;
 // Пишем содержимое обратно в файл
 file_put_contents($file, $current);
+*/
+function next_day(){
 
-    preg_match('/USD<br\/>(.*?)<\/div>/', $out, $matches);
+if( $curl = curl_init() ) {
+    $uagent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)";
+    curl_setopt($curl, CURLOPT_URL, 'http://flights.flydubai.com/en/results/');
+    curl_setopt($curl, CURLOPT_HEADER, true);
+    curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
+    curl_setopt($curl, CURLOPT_USERAGENT, $uagent);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, "coo.txt");
+    curl_setopt($curl, CURLOPT_COOKIEFILE,"coo.txt");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    $out = curl_exec($curl);
+    preg_match('/RT\(([0-9]+)/', $out, $matches);
+    curl_close($curl);
+   } 
+//echo 'http://flights.flydubai.com/en/results/threedayview/?_='.$matches[1];
+if( $curl = curl_init() ) {
+    $uagent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)";
+    curl_setopt($curl, CURLOPT_URL, 'http://flights.flydubai.com/en/results/threedayview/?_='.$matches[1]);
+    curl_setopt($curl, CURLOPT_HEADER, true);
+    curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
+    curl_setopt($curl, CURLOPT_USERAGENT, $uagent);
+    curl_setopt($curl, CURLOPT_COOKIEJAR, "coo.txt");
+    curl_setopt($curl, CURLOPT_COOKIEFILE,"coo.txt");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    $html = curl_exec($curl);
+    curl_close($curl);
+   } 
+   return $html;
+}
 
-    preg_match('/TDFare_1_ODS_BEY_(.*?)\'/', $out, $d);
-  // print_r($matches);
-  // $html = str_get_html($out);
-  // $am = $html->find('.dayAmount .showdiv span',0);
-  // $am = $am->find('.showdiv span')->innertext;
+
+
+
+$day = 0;
+while ($day <= $period ) {
+    $tr = $html->find('.priceRow',0);
+
+    for($i=0; $i<7; $i++){
+        $day++;
+        if($day <= $period){
+
+        $elem = $tr->find('td',$i);
+        $am = $elem->find('.showdiv span',0);
+        if(empty($am)){
+             $am = $elem->find('.hidediv span',0);
+        }
+        $date = substr($elem->id, strripos($elem->id, "_") + 1, strlen($elem->id));
+     
 ?>
-<tr>
-    <td><?php echo $airports[$Origin];?></td>
-    <td><?php echo $airports[$Destination];?></td>
-    <td><?php echo $d[1];?></td>
-    <td><?php echo $period;?></td>
-    <td><?php //echo $second_date;?></td>
-    <td><?php echo $matches[1];?></td>
+            <tr>
+                <td><?php echo $airports[$Origin];?></td>
+                <td><?php echo $airports[$Destination];?></td>
+                <td><?php echo $date;?></td>
+                <td><?php echo $period;?></td>
+                <td><?php //echo $second_date;?></td>
+                <td><?php echo !empty($am)? $am : 'Рейс не найден';?></td>
 
-</tr>
-<?php
+            </tr>
+<?php 
+    
+        }else{
+            break;
+        }
+    }
+
+    $date = new DateTime($first_date);
+    $first_date = $date->format('d');
+    $first_date.= '/'.$date->format('m');
+    $first_date.= '/'.$date->format('Y');
+    $date = new DateTime($first_date);
+    $date->modify('+7 day');
+    $first_date = $date->format('d/m/Y');
+
+    $postdata = 'roundSingle=on&';
+    $postdata.= 'FormModel.Origin='.$airports[$Origin].'&';
+    $postdata.= 'FormModel.OriginAirportCode='.$Origin.'&';
+    $postdata.= 'FormModel.Destination='.$airports[$Destination].'&';
+    $postdata.= 'FormModel.DestinationAirportCode='.$Destination.'&';
+    $postdata.= 'txtDepartureDate='.$first_date.'&';
+    $postdata.= 'FormModel.DepartureDate='.$first_date.'&';
+    $postdata.= 'txtReturnDate='.$ReturnDate.'&';
+    $postdata.= 'FormModel.ReturnDate='.$ReturnDate.'&';
+    $postdata.= 'FormModel.IsFlexibleOnDates=false&';
+    $postdata.= 'FormModel.Adults=1&';
+    $postdata.= 'FormModel.Children=0&';
+    $postdata.= 'FormModel.Infants=0&';
+    $postdata.= 'FormModel.PromoCode=&';
+    $postdata.= 'flightSearch=Show+flights';
+    //$postdata = urlencode($postdata);
+    $html = post_content('http://flights.flydubai.com/en/flights/search/', $postdata);
+
+    $html = next_day();
+    $html = str_get_html($html);
+}
+
 
 ?>
